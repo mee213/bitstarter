57b5859fd6c7505b0f22dfcfc1af01dbc3a17901
Marlo Evans <evans.marlo@gmail.com>
2013-07-14 02:55:57 +0000

    Fixed web.js

--- web.js
+++ web.js
@@ -2,16 +2,14 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var infile = "index.html";
 var buf = new Buffer;
 
-fs.readFileSync('index.html', function (err, buf) {
-    if (err) {
-	return console.log(err);
-    }
-    app.get('/', function(request, response) {
-	response.send(buf.toString);
+buf = fs.readFileSync(infile);
+
+app.get('/', function(request, response) {
+	response.send(buf.toString());
     });
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
