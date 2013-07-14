8ac6e882e3cc5012583d74bc2b9b9f2f49c0d584
Marlo Evans <evans.marlo@gmail.com>
2013-07-14 03:20:45 +0000

    trying again with web.js

--- web.js
+++ web.js
@@ -2,14 +2,11 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var infile = "./index.html";
-var buf = new Buffer();
-
-buf = fs.readFileSync(infile);
 
 app.get('/', function(request, response) {
-	response.send(buf.toString());
-    });
+    var buf = fs.readFileSync('index.html');
+    response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
