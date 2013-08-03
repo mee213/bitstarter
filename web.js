f60ddb4303bf14574b87ffb7c741738ef4bf6ba0
mee213 <evans.marlo@gmail.com>
2013-08-03 23:12:20 +0000

    Testing Stripe button

--- web.js
+++ web.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var fs = require('fs');
+var stripeApiKey = "sk_test_2WkTa4068RXv3nU7SAgRH7FW";
+var stripe = require('stripe')(stripeApiKey);
 
 var app = express.createServer(express.logger());
+
+app.use(express.bodyParser());
 
 app.get('/', function(request, response) {
     var buf = fs.readFileSync('index.html');
@@ -11,4 +15,24 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+app.post('/', function(request, response) {
+    stripe.charges.create({
+	card : request.body.stripeToken,
+	amount : 3000,
+	currency : "usd"
+    }, function (err, res) {
+	if (err) {
+	    console.log("Token is " + request.body.stripeToken);
+	    console.log("Body is " + request.query);
+	    console.log(err.message);
+	    response.send(err.message);
+	} else {
+	    console.log("Token is " + request.body.stripeToken);
+	    console.log("Body is " + request.query);
+	    response.send('Your card has been successfully charged. Thank you!');
+	}
+    });
 });
